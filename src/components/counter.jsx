35c6118d0b9c872891ec34fa11cad74c96b0b84f
Andrew Le <andrewle.bc@gmail.com>
2020-03-18 21:21:07 -0700

    dodod

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,43 +2,37 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
-
-  constructor() {
-    super();
-  }
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 
-  handleIncrement = product => {
-    let state = this.state;
-    state.count++;
-    this.setState(state);
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
