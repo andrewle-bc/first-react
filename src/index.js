cb9e2572c288760bdf4e7b57e51b085b4d16eea3
Andrew Le <andrewle.bc@gmail.com>
2020-03-18 19:25:58 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from "./serviceWorker";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World!</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
+serviceWorker.unregister();
